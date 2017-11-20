3e120ba5ed49ca7600a101acd544b33abe855d85
aendel <fvattiato@gmail.com>
2017-11-20 16:23:51 +0100

    Introduzione librerie Requests e BeautifulSoup4

--- Final Project/1 - Requests.py
+++ Final Project/1 - Requests.py
@@ -1,4 +1,24 @@
+"""
+Richiesta del contenuto html di un sito web.
+"""
+#pip3 install requests
 import requests
 
-response = requests.get('https://en.wikipedia.org/wiki/Dead_Parrot_sketch')
-print(response.text)
+response = requests.get('https://it.wikipedia.org/wiki/Pagina_principale')
+#print(response.text)
+#print(type(response.text))
+
+"""
+Vogliamo manipolare l'html ottenuto (come str) dalla request tramite una nuova
+libreria html5lib
+"""
+
+html = response.text
+
+#pip3 install beautifulsoup4
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+print(soup.title)
+
+
