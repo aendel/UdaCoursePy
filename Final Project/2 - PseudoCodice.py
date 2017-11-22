0c97bc4fcb4988833adba1fe6408096f4dfcdd19
aendel <fvattiato@gmail.com>
2017-11-22 09:05:36 +0100

    Aggiunta pseudocodice

---
+++ Final Project/2 - PseudoCodice.py
@@ -0,0 +1,12 @@
+"""
+
+page = a random starting page
+article_chain = []
+while title of page isn't 'Philosophy' and we have not discovered a cycle:
+    append page to article_chain
+    download the page content
+    find the first link in the content
+    page = that link
+    pause for a second
+
+"""
