c4b14c589f0eb49e41e326f3082e43072c74915f
aendel <fvattiato@gmail.com>
2017-11-23 12:36:00 +0100

    Aggiunta funzione ricerca

---
+++ Final Project/3 - While Ricerca.py
@@ -0,0 +1,21 @@
+# TODO: Implement the continue_crawl function described above
+def continue_crawl(history,target):
+    if history[-1] == target:
+        print("Obiettivo trovato! Concludere.")
+        return False
+    elif len(history) > 25:
+        print("Ricerca troppo lunga. Concludere.")
+        return False
+        
+        #history[-1] -> intendiamo l'ultimo articolo.
+        #in -> intendiamo la presenza all'interno della lista.
+        #history[:-1] -> intendiamo la lista escluso l'ultimo elemento.
+        
+    elif history[-1] in history[:-1]:
+        print("Ultimo articolo presente nella lista degli articoli gia visionati. Ciclo. Concludere.")
+        return False
+    else:
+        print("Continua...")
+        return True
+
+print(continue_crawl(['https://en.wikipedia.org/wiki/Floating_point'],'https://en.wikipedia.org/wiki/Philosophy'))
